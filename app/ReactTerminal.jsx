41232a307fd6f82a28bd9e9b5b3d157a6a5b0b79
lenmorld <lenmorld@gmail.com>
2019-03-23 21:59:01 -0400

    command history

--- app/ReactTerminal.jsx
+++ app/ReactTerminal.jsx
@@ -2,21 +2,36 @@ import React from "react";
 
 class ReactTerminal extends React.Component {
 
+    constructor() {
+       super();
+       
+       this.state = {
+           prompt: "lenny > ",
+           commandHistory: []
+       }
+    }
+
     componentDidMount() {
         this.nameInput.focus();
     }
 
     sendCommand(event) {
         if (event.keyCode === 13) {
-            alert(event.target.value);
+            this.setState({
+                commandHistory: this.state.commandHistory.concat(event.target.value)
+            })
+            // alert(event.target.value);
         }
     }
 
     render() {
         return (
             <div>
+                { this.state.commandHistory.map(c => {
+                    return <div>{this.state.prompt} {c}</div>
+                }) }
                 <div className="terminal">
-                    {"lenny > "}
+                    {this.state.prompt}
                     <span className="cursor">
                         <input type="text"
                             ref={(input) => { this.nameInput = input; }}
