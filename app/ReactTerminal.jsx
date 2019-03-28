d70bf1e986a8c0746d87decbf0841674433bb6d3
lenmorld <lenmorld@gmail.com>
2019-03-27 21:26:08 -0400

    first stab at command log + history

--- app/ReactTerminal.jsx
+++ app/ReactTerminal.jsx
@@ -7,7 +7,8 @@ class ReactTerminal extends React.Component {
        
        this.state = {
            prompt: "lenny > ",
-           commandHistory: []
+           commandHistory: [],
+           completeLog: []
        }
     }
 
@@ -15,11 +16,21 @@ class ReactTerminal extends React.Component {
         this.nameInput.focus();
     }
 
+    processCommand(command) {
+        return `>> ${command} -> ${command.toUpperCase()}`;
+    }
+
     sendCommand(event) {
         if (event.keyCode === 13) {
+            const command = event.target.value;
+            const result = this.processCommand(command);
+
             this.setState({
-                commandHistory: this.state.commandHistory.concat(event.target.value)
+                commandHistory: this.state.commandHistory.concat(command),
+                completeLog: this.state.completeLog.concat(result)
             })
+
+            this.nameInput.value = "";
             // alert(event.target.value);
         }
     }
@@ -27,8 +38,13 @@ class ReactTerminal extends React.Component {
     render() {
         return (
             <div>
+                {/* commandHistory */}
                 { this.state.commandHistory.map(c => {
                     return <div>{this.state.prompt} {c}</div>
+                }) }
+
+                { this.state.completeLog.map(c => {
+                    return <div><p>{c}</p></div>
                 }) }
                 <div className="terminal">
                     {this.state.prompt}
@@ -39,6 +55,7 @@ class ReactTerminal extends React.Component {
                             onKeyUp={(event) => this.sendCommand(event)} />
                         <i></i>
                     </span>
+
                 </div>
                 {/* <a href="./index2.html">OLD SITE</a> */}
             </div>
